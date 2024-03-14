24f1a76eeb891e34e190d85a4c6c655d3ef9566f
aleks <aleks_20905@mail.bg>
2024-03-14 18:20:29 +0200

    plasma 6 unsteble

--- configuration.nix
+++ configuration.nix
@@ -51,7 +51,7 @@ nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   # Enable the KDE Plasma Desktop Environment.
   services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
+  services.xserver.desktopManager.plasma6.enable = true;
 
   # Configure keymap in X11
   services.xserver = {

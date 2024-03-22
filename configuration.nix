41bae08255b6c2f927e1a0fade12a31ea270894f
aleks <aleks_20905@mail.bg>
2024-03-22 17:30:48 +0200

    pg admin added and postgresql config edits

--- configuration.nix
+++ configuration.nix
@@ -122,6 +122,7 @@ nix.settings.experimental-features = [ "nix-command" "flakes" ];
   gdb
   stdenv
   p7zip
+  pgadmin4
 
   #libsForQt5.polonium #kinda workd but cant get full crean crashesh and etc kinda poor exepriance
 
@@ -144,6 +145,11 @@ nix.settings.experimental-features = [ "nix-command" "flakes" ];
     authentication = pkgs.lib.mkOverride 10 ''
           #type database  DBuser  auth-method
           local all       all     trust
+          #type database DBuser origin-address auth-method
+          # ipv4
+          host  all      all     127.0.0.1/32   trust
+          # ipv6
+          host all       all     ::1/128        trust
         '';
   };
 

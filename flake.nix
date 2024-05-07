5d9dda33fb8f4ab223b9a53df423d4269e0e4da2
aleks <aleks_20905@mail.bg>
2024-05-07 16:37:36 +0300

    add pen tools.nix

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
       nixosConfigurations.default = nixpkgs.lib.nixosSystem {
           specialArgs = {inherit inputs;};
           modules = [ 
-            
+            ./penTools.nix
             ./configuration.nix
             inputs.home-manager.nixosModules.default
           ];

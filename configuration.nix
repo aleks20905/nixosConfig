c946a6a9d69c4b5ab4671eaf7f12582e7a381b24
aleks <aleks20905@mail.bg>
2024-06-11 17:27:31 +0300

    wayland

--- configuration.nix
+++ configuration.nix
@@ -43,7 +43,7 @@ nix.settings.experimental-features = [ "nix-command" "flakes" ];
   services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable   = true;
+  services.displayManager.sddm.wayland.enable = true;
   services.desktopManager.plasma6.enable = true;
   
   # Configure keymap in X11

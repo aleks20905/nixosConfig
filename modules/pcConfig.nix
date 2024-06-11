94c23d3118fa6eba5db4a0b3f31a668b00b7ae86
aleks <aleks20905@mail.bg>
2024-06-11 18:02:37 +0300

    amd drivers fix prob...

--- modules/pcConfig.nix
+++ modules/pcConfig.nix
@@ -18,4 +18,21 @@
         dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     };
 
+
+
+    boot.initrd.kernelModules = [ "dm-snapshot" "amdgpu" ];
+
+    services.xserver.enable =true;
+    services.xserver.videoDrivers = [ "amdgpu" ];
+    services.xserver.deviceSection = ''
+            Option "DRI" "3"
+            Option "VariableRefresh" "true"
+    '';
+    hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+    };
+    
+
 }

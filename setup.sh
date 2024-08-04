405c4a42cd814a66dc5b78eb896fbf17dde85f72
aleks <aleks20905@mail.bg>
2024-08-04 16:24:32 +0300

    [added] when updatest print new packages and versions

--- setup.sh
+++ setup.sh
@@ -15,6 +15,17 @@ update_nix() {
     sudo nix flake update 
 }
 
+what_happend() {
+    nix store diff-closures /run/*-system
+
+}
+
+what_happend_all() {
+    nix profile diff-closures --profile /nix/var/nix/profiles/system
+
+}
+
+
 # Main function to display menu and handle user input
 main() {
     echo "Choose mode to switch:"
@@ -22,6 +33,8 @@ main() {
     echo "2. Laptop Mode"
     echo "3. PC Mode"
     echo "4. flake update"
+    echo "5. whats happend "
+    echo "6. whats happend - ALL"
     echo "0. Exit"
     read -p "Enter your choice: " choice
 
@@ -30,6 +43,8 @@ main() {
         2) switch_mode "$script_dir#laptop" ;;
         3) switch_mode "$script_dir#pc" ;;
         4) update_nix ;;
+        5) what_happend ;;
+        5) what_happend_all ;;
         0) echo "Exiting..."; exit ;;
         *) echo "Invalid choice. Please choose again." ;;
     esac

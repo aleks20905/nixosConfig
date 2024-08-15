afa6be393efc73bad16644167b0f32f6f6b27019
aleks <aleks20905@mail.bg>
2024-08-15 16:00:51 +0300

    [test#2] plasma manger test 2

--- flake.nix
+++ flake.nix
@@ -8,10 +8,16 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    plasma-manager = {
+      url = "github:nix-community/plasma-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs:
+  outputs = { self, nixpkgs, plasma-manager, ... }@inputs:
     let
+      username = "aleks";
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in
@@ -50,7 +56,15 @@
             ./modules/db.nix
             ./modules/standart.nix
             ./configuration.nix
-            inputs.home-manager.nixosModules.default
+            inputs.home-manager.nixosModules.home-manager {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.sharedModules = [ plasma-manager.homeManagerModules.plasma-manager ];
+
+              # This should point to your home.nix path of course. For an example
+              # of this see ./home.nix in this directory.
+              home-manager.users."${username}" = import ./home.nix;
+            }
           ];
         };
       };

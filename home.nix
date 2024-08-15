146090e2b7b39d1106827b6e139b2d43706b57b6
aleks <aleks20905@mail.bg>
2024-08-15 16:00:51 +0300

    [test#2] plasma manger test 2

--- home.nix
+++ home.nix
@@ -1,6 +1,8 @@
-{ config, pkgs, ... }:
+{ config, pkgs,  ... }:
 
 {
+
+  imports = [ ./plasma_conf.nix ];
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "aleks";

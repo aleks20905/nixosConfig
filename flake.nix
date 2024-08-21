73ff5901a77f6658d6de434b77f2b6f480c4ad93
Alekandar Angelov <aleks_20905@mail.bg>
2024-08-22 01:33:26 +0300

    [feat] flake rewrite

--- flake.nix
+++ flake.nix
@@ -20,60 +20,52 @@
       username = "aleks";
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
+
+      commonModules = [
+        ./modules/db.nix
+        ./modules/standart.nix
+        ./configuration.nix
+      ];
+
+      nixosConfig = { hardwareModule, extraModules, userConfigFile }:
+        nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs; };
+          modules = [
+            hardwareModule
+          ] ++ commonModules ++ extraModules ++ [
+            inputs.home-manager.nixosModules.home-manager {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.sharedModules = [ plasma-manager.homeManagerModules.plasma-manager ];
+              home-manager.users."${username}" = import userConfigFile;
+            }
+          ];
+        };
     in
     {
       nixosConfigurations = {
-        default = nixpkgs.lib.nixosSystem {
-          specialArgs = {inherit inputs;};
-          modules = [ 
-            # ./modules/penTools.nix
-            ./modules/db.nix
-            ./modules/standart.nix
-            ./configuration.nix
-            inputs.home-manager.nixosModules.default
-          ];
+        default = nixosConfig {
+          hardwareModule = null;  # No specific hardware module for default
+          extraModules = [];
+          userConfigFile = ./home.nix;
         };
 
-        laptop = nixpkgs.lib.nixosSystem {
-          specialArgs = {inherit inputs;};
-          modules = [ 
-            ./hardware-modules/hardware-laptop.nix
+        laptop = nixosConfig {
+          hardwareModule = ./hardware-modules/hardware-laptop.nix;
+          extraModules = [
             ./modules/laptopConfig.nix
             ./modules/penTools.nix
-            ./modules/db.nix
-            ./modules/standart.nix
-            ./configuration.nix
-            inputs.home-manager.nixosModules.home-manager {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.sharedModules = [ plasma-manager.homeManagerModules.plasma-manager ];
-
-              # This should point to your home.nix path of course. For an example
-              # of this see ./home.nix in this directory.
-              home-manager.users."${username}" = import ./home.nix;
-            }
           ];
+          userConfigFile = ./home.nix;
         };
 
-        pc = nixpkgs.lib.nixosSystem {
-          specialArgs = {inherit inputs;};
-          modules = [ 
-            ./hardware-modules/hardware-pc.nix
+        pc = nixosConfig {
+          hardwareModule = ./hardware-modules/hardware-pc.nix;
+          extraModules = [
             ./modules/pcConfig.nix
             # ./modules/penTools.nix
-            ./modules/db.nix
-            ./modules/standart.nix
-            ./configuration.nix
-            inputs.home-manager.nixosModules.home-manager {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.sharedModules = [ plasma-manager.homeManagerModules.plasma-manager ];
-
-              # This should point to your home.nix path of course. For an example
-              # of this see ./home.nix in this directory.
-              home-manager.users."${username}" = import ./home.nix;
-            }
           ];
+          userConfigFile = ./home.nix;
         };
       };
     };

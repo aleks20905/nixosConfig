79c2694f0949209f85d73327733bee1f55733e21
aleks <aleks20905@mail.bg>
2024-11-25 16:30:32 +0200

    [flake] flake + kitty

--- home.nix
+++ home.nix
@@ -62,6 +62,12 @@
   programs.kitty = {
   	enable = true;
   	settings = {
+      dynamic_background_opacity = true;
+      enable_audio_bell = false;
+      mouse_hide_wait = "-1.0";
+      window_padding_width = -5;
+      background_opacity = "0.5";
+      background_blur = 5; 
   		confirm_os_window_close = -0;
   		copy_on_select = true;
   		clipboard_control = "write-clipboard read-clipboard write-primary read-primary";

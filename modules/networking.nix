a70d01fe1c35d30e0460c250f6a10229728c79a6
aleks <aleks20905@mail.bg>
2024-12-05 21:36:19 +0200

    remove duplicates

--- modules/networking.nix
+++ modules/networking.nix
@@ -23,10 +23,6 @@
     # networking.proxy.default = "http://user:password@proxy:port/";
     # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-    # Enable networking
-    networking.networkmanager.enable = true;
-
-
     # Open ports in the firewall.
     # networking.firewall.allowedTCPPorts = [ ... ];
     # networking.firewall.allowedUDPPorts = [ ... ];

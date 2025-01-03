6568b867f77ca49cae8d15f243a2ffc97fd218da
aleks <aleks_20905@mail.bg>
2025-01-03 20:55:31 +0200

    git global ignore [env, vscode]

--- home.nix
+++ home.nix
@@ -55,6 +55,11 @@
   	enable = true;
   	userName = "aleks";
   	userEmail = "aleks_20905@mail.bg";
+    ignores = [
+      ".env "
+      "*.env"
+      ".vscode"
+    ];
   };
 
   programs.kitty = {

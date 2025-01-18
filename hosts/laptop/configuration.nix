30530b93fb056f7a90c580fc3257280b372345e7
aleks <aleks_20905@mail.bg>
2025-01-19 00:00:19 +0200

    testing

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -4,6 +4,7 @@
 {
   imports = [ 
     ../common
+    ./modules/powerManagment.nix
   ];
   
   home-manager = {
@@ -13,27 +14,11 @@
     };
   };
 
+
+  # disables network-wait-online so that it boots faster shoud not make any problems !!! but its posible to cause some services to break !!! 
+  systemd.services.NetworkManager-wait-online.enable = false;
  
-  # addes partition manager 
-  # programs.partition-manager.enable = true;
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.11"; # Did you read the comment?
 }
 

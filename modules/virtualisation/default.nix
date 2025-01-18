6f50c05975789fca974e191e43bb69f9640c9c5e
aleks <aleks_20905@mail.bg>
2025-01-18 23:43:29 +0200

    test

--- modules/virtualisation/default.nix
+++ modules/virtualisation/default.nix
@@ -1,10 +1,14 @@
-{config, lib, ...}: {
+{config, pkgs, lib, ...}: {
 
     # Docker stuff rootles etc ... 
     virtualisation.docker.rootless = {
         enable = true;
         setSocketVariable = true;
     };
- 
+
+    environment.systemPackages = with pkgs;[
+
+        # nixpacks # some type of conteriner shiet idk 'shity version of docker '
+    ] 
 
 }

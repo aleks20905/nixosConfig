cc02fcb9944bc69c3af3fe961414bca0cf07364f
aleks <aleks_20905@mail.bg>
2025-01-18 21:47:11 +0200

    testing

---
+++ modules/gaming/default.nix
@@ -0,0 +1,28 @@
+{config, lib, ...}:{
+
+    programs.steam = {
+        enable = true;
+        gamescopeSession.enable = true;
+        remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+        dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    };
+
+    programs.gamemode.enable = true;
+
+    environment.systemPackages = with pkgs; [
+
+        protonup # thing to download porton 
+
+    ];
+
+    environment.sessionVariables = {
+        STEAM_EXTRA_COMPAT_TOOLS_PATHS =
+        "\${HOME}/.steam/root/compatibilitytools.d";
+    };
+
+    # options that u can run with steam
+    # gamemoderun %command%
+    # mangohud %command%
+    # gamescope %command%
+}
+

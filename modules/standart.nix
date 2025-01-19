961b586e07dc7203937a3a5f358cdd4caff894e4
aleks <aleks_20905@mail.bg>
2025-01-19 22:57:52 +0200

    format

--- modules/standart.nix
+++ modules/standart.nix
@@ -2,16 +2,15 @@
 
 {
  
-
     imports = [ 
-       ./applications/default.nix 
-       ./fonts/default.nix 
-       ./gaming/default.nix 
-    #    ./penTools/default.nix 
-       ./programming/default.nix 
-       ./virtualisation/default.nix 
-       ./zsh/default.nix 
 
+       ./applications 
+       ./fonts
+       ./gaming
+    #    ./penTools
+       ./programming
+       ./virtualisation
+       ./zsh
     ];
 
    
@@ -19,21 +18,15 @@
     environment.systemPackages = with pkgs; [
 
        
-
         #------- Console Utilities -------
         # toybox # problem bugs and cant use ls and etc 
         ripgrep
         lf  
         fd
         #------- Console Utilities -------
-
-
-
-
     ];
-
-
 
     # Enable CUPS to print documents.
     # services.printing.enable = true;
+
 }

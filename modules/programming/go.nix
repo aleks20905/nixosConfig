5c75911e2f9a665213c056223c0d7d53063db840
aleks <aleks_20905@mail.bg>
2025-01-19 22:25:14 +0200

    c-dev adde

---
+++ modules/programming/go.nix
@@ -0,0 +1,16 @@
+{pkgs, ...}:{
+
+    environment.systemPackages = with pkgs; [
+
+        go 
+
+        # gosec # brutfocess some security problems for golang apps 
+
+        templ
+
+        air
+
+
+    ];
+
+}

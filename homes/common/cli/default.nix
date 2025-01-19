192d19dc4989a29471c0679a834c8d1e0a3e69a1
aleks <aleks_20905@mail.bg>
2025-01-19 21:49:21 +0200

    test

--- homes/common/cli/default.nix
+++ homes/common/cli/default.nix
@@ -1,6 +1,8 @@
 {
 
   imports = [
+    ./btop.nix
+    ./fastfetch.nix
     ./git.nix
   ];
 
--- homes/common/terminals/kitty.nix
+++ homes/common/terminals/kitty.nix
@@ -1,26 +1,26 @@
-{
+{ pkgs, config, ... }: {
 
-programs.kitty = {
-  	enable = true;
-    font = {
-        name = "FantasqueSansM Nerd Font Mono";
-        size = 12;
+    programs.kitty = {
+        enable = true;
+
+        font = {
+            name = "FantasqueSansM Nerd Font Mono";
+            size = 12;
+        };
+
+        settings = {
+            dynamic_background_opacity = true;
+            enable_audio_bell = false;
+            mouse_hide_wait = "-1.0";
+            # window_padding_width = -5;
+            background_opacity = "0.5";
+            background_blur = 5; 
+            confirm_os_window_close = -0;
+            copy_on_select = true;
+            clipboard_control = "write-clipboard read-clipboard write-primary read-primary";
+            # font_family = "FantasqueSansMono Nerd Font";
+        };
     };
-
-  	settings = {
-      dynamic_background_opacity = true;
-      enable_audio_bell = false;
-      mouse_hide_wait = "-1.0";
-      # window_padding_width = -5;
-      background_opacity = "0.5";
-      background_blur = 5; 
-  		confirm_os_window_close = -0;
-  		copy_on_select = true;
-  		clipboard_control = "write-clipboard read-clipboard write-primary read-primary";
-  	  # font_family = "FantasqueSansMono Nerd Font";
-
-    };
-  };
  
 
 

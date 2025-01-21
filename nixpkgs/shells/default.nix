4773ec47d1e11986d978c34b2833150c1e6f3372
aleks <aleks_20905@mail.bg>
2025-01-21 21:06:44 +0200

    removed db.nix havent used it in ages no point in keeping it

--- nixpkgs/shells/default.nix
+++ nixpkgs/shells/default.nix
@@ -6,7 +6,7 @@
     users.defaultUserShell=pkgs.zsh; # needed
     programs.zsh = {
         enable = true;
-        autosuggestions.enable = true;
+        autosuggestions.enable = true;      #it doest have the same name from home-manager or doest work at all idk 
         zsh-autoenv.enable = true;
         syntaxHighlighting.enable = true;
         ohMyZsh = {

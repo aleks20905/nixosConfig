63e2c34c1b02634fde4db6e31745af508e7147f9
aleks <aleks_20905@mail.bg>
2025-01-22 02:22:50 +0200

    added spicetify flake

--- flake.nix
+++ flake.nix
@@ -9,8 +9,8 @@
 		home-manager.url = "github:nix-community/home-manager";
 		home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-		# nix-spicetify.url = "github:the-argus/spicetify-nix";
-		# nix-spicetify.inputs.nixpkgs.follows = "nixpkgs";
+		spicetify-nix.url = "github:Gerg-L/spicetify-nix";
+		spicetify-nix.inputs.nixpkgs.follows = "nixpkgs";
 
 	};
 

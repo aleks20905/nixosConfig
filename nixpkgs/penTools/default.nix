793ececbdf932bd489f51d3190d1f85ece19ce0b
aleks <aleks_20905@mail.bg>
2025-01-23 22:41:46 +0200

    testing options config

--- nixpkgs/penTools/default.nix
+++ nixpkgs/penTools/default.nix
@@ -1,28 +1,35 @@
-{ config, pkgs, ...}:
+{ config, lib, pkgs, ...}:
 
 {
-  
+    options = {
 
+        pentools.enable = lib.mkEnableOption "enables pentools";
+
+    };
+
+    config = {
     environment.systemPackages = with pkgs; [
     
-    aircrack-ng # airmon-ng   create new global template network security or smt
-    wireshark # create new global template network security or smt
-    wifite2
+        aircrack-ng # airmon-ng   create new global template network security or smt
+        wireshark # create new global template network security or smt
+        wifite2
 
-    # wordlists   
+        # wordlists   
 
-    wordlists # rockyou package create new global template network security or smt
-    (wordlists.override {
-        lists = [ rockyou dirbuster];
-    })
-    
-    exploitdb
-    nmap
-    gobuster
-    nikto
-    thc-hydra
-    sqlmap
+        wordlists # rockyou package create new global template network security or smt
+        (wordlists.override {
+            lists = [ rockyou dirbuster];
+        })
+        
+        exploitdb
+        nmap
+        gobuster
+        nikto
+        thc-hydra
+        sqlmap
 
     ];
+    };
+
 
 }
--- nixpkgs/standart.nix
+++ nixpkgs/standart.nix
@@ -1,30 +1,19 @@
 { config, lib, pkgs, ...}:
 
 {
-    options = {
-
-        # Define options for enabling/disabling modules
-        enableGaming = lib.mkEnableOption "Enable gaming-related packages "                 // { default = true; }; 
-        enableProgramming = lib.mkEnableOption "Enable programming-related packages "       // { default = true; }; 
-        enablePentools = lib.mkEnableOption "Enable pentesting tools"                       // { default = false; };
-        enableVirtualisation = lib.mkEnableOption "Enable virtualisation-related packages " // { default = true; }; 
-    }; 
 
     imports = [ 
 
         ./fonts
-    #    ./gaming
-    #    ./penTools
-    #    ./programming
+       ./gaming
+       ./penTools
+       ./programming
         ./shells
-    #    ./virtualisation
+       ./virtualisation
 
-        (lib.mkIf config.enableGaming ./gaming)
-        (lib.mkIf config.enableProgramming ./programming)
-        (lib.mkIf config.enablePentools ./pentools)
-        (lib.mkIf config.enableVirtualisation ./virtualisation)
     ];
 
+    pentools.enable = false;
    
     
 

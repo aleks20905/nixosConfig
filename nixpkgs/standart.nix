88f8de39099832f40e36cc078b2e30665c9f6668
aleks <aleks_20905@mail.bg>
2025-01-23 22:24:40 +0200

    testing with nix Options

--- nixpkgs/standart.nix
+++ nixpkgs/standart.nix
@@ -1,15 +1,28 @@
-{ config, pkgs, ...}:
+{ config, lib, pkgs, ...}:
 
 {
- 
+    options = {
+
+        # Define options for enabling/disabling modules
+        enableGaming = lib.mkEnableOption "Enable gaming-related packages "                 // { default = true; }; 
+        enableProgramming = lib.mkEnableOption "Enable programming-related packages "       // { default = true; }; 
+        enablePentools = lib.mkEnableOption "Enable pentesting tools"                       // { default = false; };
+        enableVirtualisation = lib.mkEnableOption "Enable virtualisation-related packages " // { default = true; }; 
+    }; 
+
     imports = [ 
 
-       ./fonts
-       ./gaming
+        ./fonts
+    #    ./gaming
     #    ./penTools
-       ./programming
-       ./virtualisation
-       ./shells
+    #    ./programming
+        ./shells
+    #    ./virtualisation
+
+        (lib.mkIf config.enableGaming ./gaming)
+        (lib.mkIf config.enableProgramming ./programming)
+        (lib.mkIf config.enablePentools ./pentools)
+        (lib.mkIf config.enableVirtualisation ./virtualisation)
     ];
 
    

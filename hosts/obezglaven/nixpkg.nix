ee9c17d11095005867da67029ffc3c0393e4c5cd
aleks <aleks_20905@mail.bg>
2025-02-04 15:27:33 +0200

    created obezglaven config [only preparations]

---
+++ hosts/obezglaven/nixpkg.nix
@@ -0,0 +1,21 @@
+{
+	imports = [ 
+
+
+        ../../nixpkgs/fonts
+        # ../../nixpkgs/gaming
+        # ../../nixpkgs/penTools
+        # ../../nixpkgs/programming
+        ../../nixpkgs/shells
+        # ../../nixpkgs/virtualisation # for new not needed 
+
+
+
+
+		# ../../nixpkgs/standart.nix # shoud not be used [shoud be removed]
+
+
+	];
+   
+} 
+ 

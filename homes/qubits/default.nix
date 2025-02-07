174d019e7f5985789fa7b479c65867a2c08495d1
aleks <aleks_20905@mail.bg>
2025-02-07 18:42:02 +0200

    idk

---
+++ homes/qubits/default.nix
@@ -0,0 +1,24 @@
+{...}: {
+  imports = [
+    ../common
+  ];
+
+  nixpkgs = {
+    
+
+    config = {
+      allowUnfree = true;
+      allowUnfreePredicate = _: true;
+    };
+  };
+
+  home.username = "qubits";
+  home.homeDirectory = "/home/qubits";
+
+
+  systemd.user.startServices = "sd-switch";
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  home.stateVersion = "24.11"; # Please read the comment before changing.
+}
+

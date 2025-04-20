1400fa2d47f41689bf1b865d46d627fb21d5ecf6
aleks <aleks_20905@mail.bg>
2025-04-20 13:50:00 +0300

    nvim auto pull config if not present

--- homes/common/applications/editors/nvim.nix
+++ homes/common/applications/editors/nvim.nix
@@ -1,10 +1,23 @@
-{pkgs, ...}:{
+{pkgs, lib, ...}:{
 
 # nvim configuration 
 
+    programs.neovim = {
+
+        enable = true;
+        # extraConfig = ''
+        #     set number relativenumber
+        # '';
+
+        defaultEditor = true;
+
+        viAlias = true;
+        vimAlias = true;
+    };
+
     home.packages = with pkgs;[
 
-        neovim
+        # neovim
    
         wl-clipboard
 
@@ -13,7 +26,17 @@
 
     ];
 
-
+    # each rebuild checks in the home folder for "./config/nvim" if it doest exist, it will pull it from github
+    home.activation = {
+    cloneNvimConfig = lib.hm.dag.entryAfter ["writeBoundary"] ''
+      if [ ! -d "$HOME/.config/nvim" ]; then
+        echo "Cloning Neovim config..."
+        ${pkgs.git}/bin/git clone https://github.com/aleks20905/nvim.git "$HOME/.config/nvim"
+      else
+        echo "Neovim config already exists. Skipping clone."
+      fi
+    '';
+  };
 
 }
 

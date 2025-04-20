c0c0793f048f7d9549b3a849bd40be45dd203c6e
aleks <aleks_20905@mail.bg>
2025-04-20 13:50:00 +0300

    nvim auto pull config if not present

--- homes/common/cli/git.nix
+++ homes/common/cli/git.nix
@@ -12,11 +12,14 @@
         userEmail = "aleks_20905@mail.bg";
 
         ignores = [
-        ".env "
-        "*.env"
-        ".vscode"
+            ".env "
+            "*.env"
+            ".vscode"
         ];
     };
 
 }
 
+# ssh-keygen -t ed25519 -C "aleks_20905@mail.bg" 
+# eval "$(ssh-agent -s)"                                                                                                                                                       INT ✘ 
+# ssh-add ~/.ssh/id_ed25519

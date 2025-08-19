bfd97bf88972f6224bece5b33201ec18a7980a6e
aleks <aleks_20905@mail.bg>
2025-08-19 16:59:52 +0300

    refactor removed ui based apps from obezglaven + lazygit -> gitui

--- homes/common/cli/git.nix
+++ homes/common/cli/git.nix
@@ -2,7 +2,8 @@
 
     home.packages = with pkgs; [
 
-        lazygit
+        gitui
+        # lazygit
     ];
 
     programs.git = {
--- hosts/obezglaven/home_aleks.nix
+++ hosts/obezglaven/home_aleks.nix
@@ -5,11 +5,19 @@
   imports = [
     ../../homes/aleks
     # ../../homeManagerModules/common/audio.nix
-    ../../homes/common/applications
+
+
+    # ../../homes/common/applications/browsers
+    # ../../homes/common/applications/communications
+    ../../homes/common/applications/editors
+    # ../../homes/common/applications/media
+    # ../../homes/common/applications/utils
+
+
     ../../homes/common/cli
     ../../homes/common/terminals/kitty.nix
   ];
-
+  
 
 
 

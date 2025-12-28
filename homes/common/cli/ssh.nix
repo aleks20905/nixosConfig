ed31a6e01bdb7aa6851ea5ae0de9439b45e25ed6
aleks <aleks_20905@mail.bg>
2025-12-28 17:25:05 +0200

    ssh extraconfig swaped to  matchBlock "*"

--- homes/common/cli/ssh.nix
+++ homes/common/cli/ssh.nix
@@ -1,22 +1,41 @@
-{pkgs, config, ...}:{
+{ pkgs, config, ... }: {
 
+  home.packages = with pkgs; [ cloudflared ];
 
-    home.packages = with pkgs; [ cloudflared ];
+  programs.ssh = {
+    enable = true;
+    enableDefaultConfig = false;
 
-    programs.ssh = {
-        # enable = true; # removed cuz eval warning idk mate like ff off i dont giva fuck u just want this shit to work and not scream at me for no reason stupit BS 
+    matchBlocks = {
+      "*" = { };
 
-        extraConfig = ''
-         Host aleks-ssh-quantized
-            HostName ssh.quantized.space
-            User aleks 
-            ProxyCommand cloudflared access ssh --hostname %h
+      aleks-ssh-quantized = {
+        hostname = "ssh.quantized.space";
+        user = "aleks";
+        proxyCommand = "cloudflared access ssh --hostname %h";
+      };
 
-         Host qubits-ssh-quantized
-            HostName ssh.quantized.space
-            User qubits
-            ProxyCommand cloudflared access ssh --hostname %h
-        '';
+      qubits-ssh-quantized = {
+        hostname = "ssh.quantized.space";
+        user = "qubits";
+        proxyCommand = "cloudflared access ssh --hostname %h";
+      };
+
     };
 
+  };
+
 }
+
+# just why is this shiet beein changed like helo its a normal thing can u can reuse F this 
+# extraConfig = ''
+#   Host aleks-ssh-quantized
+#     HostName ssh.quantized.space
+#     User aleks 
+#     ProxyCommand cloudflared access ssh --hostname %h
+
+#   Host qubits-ssh-quantized
+#     HostName ssh.quantized.space
+#     User qubits
+#     ProxyCommand cloudflared access ssh --hostname %h
+# '';

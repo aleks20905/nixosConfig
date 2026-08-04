fd2d6a1d351457b0339cf5db99c2246d18a5ec5b
aleks <aleks_20905@mail.bg>
2026-08-04 20:39:12 +0300

    templates nstemplates

--- templates/flake.nix
+++ templates/flake.nix
@@ -7,10 +7,43 @@
         path = ./go;
         description = "Go template";
       };
+
       python = {
         path = ./python;
         description = "python template";
       };
+
+      py = {
+        path = ./python;
+        description = "python template";
+      };
+
+      zig = {
+        path = ./zig;
+        description = "zig template";
+      };
+
+      rust = {
+        path = ./rust;
+        description = "rust template";
+      };
+
+      c = {
+        path = ./c;
+        description = "c template";
+      };
+
+      empty = {
+        path = ./empty;
+        description = "empty template";
+      };
+
+      haskell = {
+        path = ./haskell;
+        description = "haskell template";
+      };
+      
     };
   };
 }
+
